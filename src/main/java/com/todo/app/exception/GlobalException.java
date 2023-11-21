3d02078a728a422cf88862393fe3f9dec75948a1
shakthichaithanya <shakthits01@gmail.com>
2023-11-21 22:06:40 +0530

    added java docs to global exception class

--- src/main/java/com/todo/app/exception/GlobalException.java
+++ src/main/java/com/todo/app/exception/GlobalException.java
@@ -10,15 +10,31 @@ import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDate;
 
-
+/**
+ * this class will handle all exceptions
+ */
 @RestControllerAdvice
 public class GlobalException {
+    /**
+     * this method handles TaskNotCreatedException
+     *
+     * @param TasksNotCreatedException exception
+     * @param WebRequest request
+     * @return ResponseEntity
+     */
     @ExceptionHandler(TasksNotCreatedException.class)
     public ResponseEntity<ErrorInfo> TaskNotCreatedExceptionHandler(TasksNotCreatedException exception, WebRequest request) {
         ErrorInfo error = new ErrorInfo(HttpStatus.NOT_FOUND, LocalDate.now(), exception.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
+    /**
+     * this method handles MethodArgumentNotValidException
+     *
+     * @param MethodArgumentNotValidException exception
+     * @param WebRequest request
+     * @return ResponseEntity
+     */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorInfo> customConstraintViolationExceptionHandler(MethodArgumentNotValidException exception, WebRequest request) {
         ErrorInfo error = new ErrorInfo(HttpStatus.BAD_REQUEST, LocalDate.now(), exception.getBindingResult().getFieldError().getDefaultMessage(), request.getDescription(false));
@@ -26,6 +42,13 @@ public class GlobalException {
     }
 
 
+    /**
+     * this method handles all exceptions it is a default exception handler
+     *
+     * @param  exception
+     * @param WebRequest request
+     * @return ResponseEntity
+     */
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorInfo> exceptionHandler(Exception exception, WebRequest request) {
         ErrorInfo error = new ErrorInfo(HttpStatus.INTERNAL_SERVER_ERROR, LocalDate.now(), exception.getMessage(), request.getDescription(false));
